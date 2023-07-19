cafceb6f2b8421a641fd4fbd704d668599c37642
davi dias tanaka <davitanaka0603@gmail.com>
2023-07-18 22:17:32 -0300

    Initial commit

---
+++ desafios/desafio02.py
@@ -0,0 +1,4 @@
+dia = input('dia =')
+mes = input('mes =')
+ano = input('Ano =')
+print('você nasceu no dia', dia, 'de', mes, 'de', ano, '. correto?')
---
+++ desafios/desafio03.py
@@ -0,0 +1,3 @@
+num1 = input('digite um numero:')
+num2 = input('digite outro numero:')
+print( "a soma dos numeros é:" num1 + num2)
