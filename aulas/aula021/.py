fdf9794297238a71e257ecfd02ece4d8eab666cc
davi dias tanaka <davitanaka0603@gmail.com>
2023-08-19 11:39:08 -0300

    correção desafios 101 a 106

--- aulas/aula021/.py
+++ aulas/aula021/.py
@@ -1,5 +1,5 @@
 def fatorial(num=1):
-    f = 1
+    f = 1 
     for c in range(num, 0, -1):
         f *= c
     return f
--- exercicios/desafio101/.py
+++ exercicios/desafio101/.py
@@ -3,7 +3,7 @@ def voto(ano):
     atual = date.today().year
     idade = atual - ano
     if idade < 16:
-        return f'Com idade {idade} NÃO VOTA'
+        return f'Com idade  {idade} NÃO VOTA'
     elif 16 <= idade < 18 or idade > 65:
         return f'Com {idade} O VOTO NÃO É OPCIONAL'
     else:
